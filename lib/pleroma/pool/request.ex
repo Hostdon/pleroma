9eede61d00daa645f8ab5314f8fe6636f0cbc4a5
sadposter <hannah+pleroma@coffee-and-dreams.uk>
2020-05-08 14:09:53 +0100

    fe

--- lib/pleroma/pool/request.ex
+++ lib/pleroma/pool/request.ex
@@ -24,42 +24,42 @@ defmodule Pleroma.Pool.Request do
   def handle_call({:execute, client, request}, _from, state) do
     response = Pleroma.HTTP.request(client, request)
 
-    {:reply, response, state}
+    {:reply, response, state, :hibernate}
   end
 
   @impl true
   def handle_info({:gun_data, _conn, _stream, _, _}, state) do
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   @impl true
   def handle_info({:gun_up, _conn, _protocol}, state) do
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   @impl true
   def handle_info({:gun_down, _conn, _protocol, _reason, _killed}, state) do
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   @impl true
   def handle_info({:gun_error, _conn, _stream, _error}, state) do
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   @impl true
   def handle_info({:gun_push, _conn, _stream, _new_stream, _method, _uri, _headers}, state) do
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   @impl true
   def handle_info({:gun_response, _conn, _stream, _, _status, _headers}, state) do
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 
   @impl true
   def handle_info(msg, state) do
     Logger.warn("Received unexpected message #{inspect(__MODULE__)} #{inspect(msg)}")
-    {:noreply, state}
+    {:noreply, state, :hibernate}
   end
 end
