774b4e165ac670d8c5f266805786f6ec3cd96090
Ekaterina Vaartis <vaartis@kotobank.ch>
2022-01-22 17:17:43 +0300

    Change search_indexing = 10 and retries for indexing = 2

--- config/config.exs
+++ config/config.exs
@@ -569,7 +569,7 @@ config :pleroma, Oban,
     attachments_cleanup: 1,
     new_users_digest: 1,
     mute_expire: 5,
-    search_indexing: 1
+    search_indexing: 10
   ],
   plugins: [Oban.Plugins.Pruner],
   crontab: [
@@ -580,7 +580,8 @@ config :pleroma, Oban,
 config :pleroma, :workers,
   retries: [
     federator_incoming: 5,
-    federator_outgoing: 5
+    federator_outgoing: 5,
+    search_indexing: 2
   ]
 
 config :pleroma, Pleroma.Formatter,
