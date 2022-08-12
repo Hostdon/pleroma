5192e21e53b32bb128b881ea0ee7caf6749575cf
FloatingGhost <hannah@coffee-and-dreams.uk>
2022-08-12 16:00:40 +0100

    bump version to 3.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pleroma.Mixfile do
   def project do
     [
       app: :pleroma,
-      version: version("3.0.1"),
+      version: version("3.1.0"),
       elixir: "~> 1.9",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
