db7ad08d1e2d4c59b52a41bb0790c073fda1649f
Norm <normandy@biribiri.dev>
2022-08-25 18:30:19 +0000

    Update min elixir version in mix.exs to 1.12
    The install docs already mention 1.12 as the minimum supported version, so this should also be reflected in `mix.exs`.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pleroma.Mixfile do
     [
       app: :pleroma,
       version: version("3.1.0"),
-      elixir: "~> 1.9",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       elixirc_options: [warnings_as_errors: warnings_as_errors()],
